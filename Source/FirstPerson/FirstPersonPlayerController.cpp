abb8127d46bb15597c55e2c7adf62f9013a772e7
WeronikaZolyniak <zolyniak.weronika@gmail.com>
2024-02-16 17:40:53 +0100

    initial commit

---
+++ Source/FirstPerson/FirstPersonPlayerController.cpp
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "FirstPersonPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+void AFirstPersonPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// get the enhanced input subsystem
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		// add the mapping context so we get controls
+		Subsystem->AddMappingContext(InputMappingContext, 0);
+
+		UE_LOG(LogTemp, Warning, TEXT("BeginPlay"));
+	}
+}
