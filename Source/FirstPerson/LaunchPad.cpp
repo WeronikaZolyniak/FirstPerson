a95f8d7492c50a86504c8b69639ed37073ec2042
WeronikaZolyniak <zolyniak.weronika@gmail.com>
2024-02-21 19:58:30 +0100

    Resolves #1

---
+++ Source/FirstPerson/LaunchPad.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LaunchPad.h"
+
+
+// Sets default values
+ALaunchPad::ALaunchPad()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	Collision = CreateDefaultSubobject<UBoxComponent>(FName("Collision"));
+	Collision->SetupAttachment(RootComponent);
+	Collision->OnComponentBeginOverlap.AddDynamic(this, &ALaunchPad::OnOverlapBegin);
+
+	UE_LOG(LogTemp, Warning, TEXT("log works"));
+
+
+
+}
+
+// Called when the game starts or when spawned
+void ALaunchPad::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+	
+}
+
+void ALaunchPad::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Overlapping Collision"));
+}
+
+
+
+// Called every frame
+void ALaunchPad::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/FirstPerson/LaunchPad.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include <Components/BoxComponent.h>
+#include "LaunchPad.generated.h"
+
+
+UCLASS()
+class FIRSTPERSON_API ALaunchPad : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALaunchPad();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp ,AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	class UBoxComponent* Collision;
+
+
+
+};
