a645584d663e463f00e4af8855bab3286fd7b9b1
WeronikaZolyniak <zolyniak.weronika@gmail.com>
2024-03-20 21:19:29 +0100

    Fixed #14 CalculateTheAngle function on compass subsystem

--- Source/FirstPerson/CompassSubsystem.cpp
+++ Source/FirstPerson/CompassSubsystem.cpp
@@ -13,13 +13,22 @@ float UCompassSubsystem::CalculateTheAngle(AActor* Actor)
 {
 	if (Actor != nullptr)
 	{
-		FVector ActorLocation = Actor->GetActorLocation();
-		float DotProduct = (ActorLocation.X * ObjectiveLocation.X) + (ActorLocation.Y * ObjectiveLocation.Y);
-		float ObjectiveMagnitude = sqrtf((ObjectiveLocation.X * ObjectiveLocation.X) + (ObjectiveLocation.Y * ObjectiveLocation.Y));
-		float ActorMagnitude = sqrtf((ActorLocation.X * ActorLocation.X) + (ActorLocation.Y * ActorLocation.Y));
-		float Quotient = DotProduct / (ObjectiveMagnitude * ActorMagnitude);
-		float Angle = FMath::RadiansToDegrees(acosf(Quotient));
-		return Angle;
+		FVector ActorForwardVector = Actor->GetActorForwardVector();
+		FVector ActorRightVector = Actor->GetActorRightVector();
+		FVector ObjectiveVector = ObjectiveLocation - Actor->GetActorLocation();
+		ObjectiveVector.Normalize();
+		float ForwardDotProduct = (ActorForwardVector.X * ObjectiveVector.X) + (ActorForwardVector.Y * ObjectiveVector.Y);
+		float RightDotProduct = (ActorRightVector.X * ObjectiveVector.X) + (ActorRightVector.Y * ObjectiveVector.Y);
+		float Angle = FMath::RadiansToDegrees(acosf(ForwardDotProduct));
+		if (RightDotProduct > 0)
+		{
+			return Angle;
+		}
+		else
+		{
+			return -Angle;
+		}
+		
 	}
 	return 0.0f;
 }
